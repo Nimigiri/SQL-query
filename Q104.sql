e637b348e6dc2e0b062d75373042e38cdc64bce7
Nimisha Giri <72227408+Nimigiri@users.noreply.github.com>
2023-06-10 02:25:12 +0530

    Create Q104.sql

---
+++ Q104.sql
@@ -0,0 +1,14 @@
+--q1
+Select firstname, lastname
+from person.person
+
+--q2
+--couldnt find company name constraint in any table 
+
+--q3
+
+
+--q11
+SELECT TOP 1 *
+FROM purchasing.PurchaseOrderHeader
+ORDER BY OrderDate ASC
